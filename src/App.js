5005833e8469e190b8e621d1979a41174464dcd2
Timothy Wang <timothywang@crescentschool.org>
2021-07-12 00:32:20 -0400

    API calls

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const fetch = require('node-fetch');
+let API_FILE = require('./API_KEY.json');
+
+let API_KEY = API_FILE["API_KEY"];
+
+const playerName = "CurrentlyKuais";
+const playerUUID = "9aa48ce9-4801-4678-b6f5-423288cddad0";
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+    }
+    this._isMounted = false;
+  }
+
+  async getJSON() {
+    await fetch(`https://api.hypixel.net/skyblock/bazaar?key=${API_KEY}&name=${playerUUID}`) // back ticks to use ${}
+      .then(res => res.json()) // take our response (in bytes) and turn it to a JSON
+      .then(data => {
+        this.setState({
+          isLoaded: true,
+          items: data,
+        });
+        console.log(data);
+      })
+      .catch(error => console.log("Network Error", error));
+  }
+
+  componentDidMount() { // this method runs after the render() method, then updates the render method
+    this._isMounted = true;
+    this._isMounted && this.getJSON();
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
+
+  render() {
+
+    let { isLoaded, item } = this.state;
+
+    if(!isLoaded) {
+      return (
+        <div>Loading...</div>
+      );
+    } else {
+      return (
+        <div className = "App">
+          <ul>
+            {/* {item.map(item => ( // map function creates a new array with the 
+                                 // result of every return in the array element
+              <li key = {item.id}>
+                Name: {item.name} | Email: {item.email}
+              </li>
+            ))}; */}
+          </ul>
+        </div>
+      );
+    }
+  }
 }
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
 
 export default App;
