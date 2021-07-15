14ba9b0e7ae8a8c86064282ac5cec40ac0ec52d8
yanbo cheng <yanbocheng01234@gmail.com>
2021-07-14 21:10:48 -0400

    implemented the search bar to actuall filter the data displayed, some bugs that have not been fixed includes the state.search to become undefined once the user presses delete or the submit button, also the .includes method is not strict with the restrictions, for example: if the string is 'something', 'omt' is technically included.

--- src/Components/Itemdisplay/Itemdisplay.js
+++ src/Components/Itemdisplay/Itemdisplay.js
@@ -1,6 +1,7 @@
 import React, {useState,useEffect} from 'react';
 import './Itemdisplay.css';
 import Item from './Item';
+import { update } from 'lodash';
 
 class Itemdisplay extends React.Component{
     constructor(props){
@@ -8,23 +9,35 @@ class Itemdisplay extends React.Component{
         this.state={
             data:this.props.dataParentToChild,
             items:[],
-            search:''
+            search:'',
+            filteredData:[]
         }
+
         this.handleChange=this.handleChange.bind(this);
         this.handleSubmit=this.handleSubmit.bind(this);
-    }S
+    }
     handleChange(event){
-        this.setState({search:event.target.value})
+        this.setState({search:event.target.value}, function(){
+            console.log(this.state.search);
+        })
+        this.setState(prevState=>{
+                const filteredData=Object.entries(prevState.data.products).filter(element=>{
+                    return element[1].product_id.toLowerCase().replace("_"," ").includes(this.state.search.toLowerCase().replace("_"," "))
+                })
+                this.setState({
+                    filteredData
+                }
+                )
+        })
     }
     handleSubmit(event){
-        alert("submitted: "+this.state.search)
-    }
+        this.setState({search:event.target.value})
+        event.preventDefault()
+
+    } 
     render(){
         const {data}=this.state
-        const test=data.products
 
-
-        console.log(test)
         return(
             <div className="Itemdisplay">
                 <div className="Item-search">
@@ -32,17 +45,15 @@ class Itemdisplay extends React.Component{
                         Search Item
                     </h1>
                     <form onSubmit={this.handleSubmit}>
-                        <input type="text" placeholder="Search" className="item-input" value={this.state.search} onChange={this.handleChange}/>
+                        <input type="text" placeholder="Search" className="item-input" onChange={this.handleChange}/>
                         <input type="submit" value="Submit" />
                     </form>
                 </div>
-                <div>
                 {
-                    Object.entries(data.products).map((item)=> 
-                        <p key={item}>{item["1"].product_id}</p>
+                    this.state.filteredData.map((item)=> 
+                        <p key={item}>{item[1].product_id}</p>
                     )
                 }
-                </div>
             </div>
 
         )
