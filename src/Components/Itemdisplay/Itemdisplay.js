09efa0a3c28ea4a9b84e27808678b48a1874d66f
yanbo cheng <yanbocheng01234@gmail.com>
2021-08-02 18:09:27 -0400

    minor fixes to values and bugs that affected sorting, made extra rows to visualize profits better, rounded numbers to 1 decimal place.

--- src/Components/Itemdisplay/Itemdisplay.js
+++ src/Components/Itemdisplay/Itemdisplay.js
@@ -11,9 +11,23 @@ class Itemdisplay extends React.Component{
             search:'',
             filteredData:[]
         }
-
         this.handleChange=this.handleChange.bind(this);
+        const sortTypes = {
+            up: {
+                class: 'sort-up',
+                fn: (a, b) => a.net_worth - b.net_worth
+            },
+            down: {
+                class: 'sort-down',
+                fn: (a, b) => b.net_worth - a.net_worth
+            },
+            default: {
+                class: 'sort',
+                fn: (a, b) => a
+            }
+        };
     }
+    
     /*handle change method for the input*/
     handleChange(event){
         /*function() to update the callback as setstate does not update the search state immediately, which will cause the search to be delayed by one mutation*/
@@ -21,7 +35,7 @@ class Itemdisplay extends React.Component{
             this.setState(prevState=>{
                 /*makes a filteredData object that will be dynamically changed as the user provides input*/
                     const filteredData=Object.entries(prevState.data.products).filter(element=>{
-                        return element[1].product_id.toString().toLowerCase().replace("_"," ").includes(this.state.search.toLowerCase().replace("_"," "))
+                        return element[1].product_id.toString().toLowerCase().replaceAll("_"," ").includes(this.state.search.toLowerCase().replace("_"," "))
                     })
                     this.setState({
                         filteredData
@@ -32,9 +46,12 @@ class Itemdisplay extends React.Component{
         /*make a filtered data to display dynamically as the user types in the search bar*/
     }
     
+    
     render(){
+        
         return(
             <div className="item-display">
+
                 <div>
                     <h1 className="item-text">
                         Search Item: 
@@ -44,12 +61,15 @@ class Itemdisplay extends React.Component{
                     </form>
                 </div>
                 <table>
+                    
                         <thead>
                             <tr>
                                 <th >Item</th>
                                 <th >Quick Buy</th>
                                 <th >Quick Sell</th>
                                 <th >7 Day Total Volume</th>
+                                <th>Profit Ratio</th>
+                                <th>Profit Difference</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -58,18 +78,23 @@ class Itemdisplay extends React.Component{
                         it will switch to the filtered data*/
                             this.state.search===""?Object.entries(this.state.data.products).map((item)=> 
                                 <tr key={item}>
+                                    {console.log(item[1].sell_summary[0]==undefined?"yes":"no")}
                                     <td ><a href={"#"+item[1].product_id} style={{ textDecoration: 'none' }}>{item[1].product_id.replaceAll("_"," ")}</a></td>
-                                    <td >{item[1].quick_status.buyPrice}</td>
-                                    <td >{item[1].quick_status.sellPrice}</td>
+                                    <td >{item[1].buy_summary[0]===undefined?"unavailable":item[1].buy_summary[0].pricePerUnit}</td>
+                                    <td >{item[1].sell_summary[0]===undefined?"unavailable":item[1].sell_summary[0].pricePerUnit}</td>
                                     <td >buy: {item[1].quick_status.buyMovingWeek} sell: {item[1].quick_status.sellMovingWeek}</td>
+                                    <td>{((item[1].quick_status.buyPrice.toFixed(1)/item[1].quick_status.sellPrice.toFixed(1))*100).toFixed(1).toString()+"%"}</td>
+                                    <td>{(item[1].quick_status.buyPrice.toFixed(1)-item[1].quick_status.sellPrice.toFixed(1)).toFixed(1)}</td>
                                 </tr>
                             ):
                             this.state.filteredData.map((item)=> 
                                 <tr key={item} className="all-items">
                                     <td ><a href={"#"+item[1].product_id} style={{ textDecoration: 'none' }}>{item[1].product_id.replaceAll("_"," ")}</a></td>
-                                    <td className="quick-buy">{item[1].quick_status.buyPrice}</td>
-                                    <td className="quick-sell">{item[1].quick_status.sellPrice}</td>
+                                    <td >{item[1].buy_summary[0]===undefined?"unavailable":item[1].buy_summary[0].pricePerUnit}</td>
+                                    <td >{item[1].sell_summary[0]===undefined?"unavailable":item[1].sell_summary[0].pricePerUnit}</td>
                                     <td className="total-buy-to-sell-volume">buy: {item[1].quick_status.buyMovingWeek} sell: {item[1].quick_status.sellMovingWeek}</td>
+                                    <td>{(item[1].quick_status.buyPrice.toFixed(1)/item[1].quick_status.sellPrice.toFixed(1)).toFixed(1)}</td>
+                                    <td>{(item[1].quick_status.buyPrice.toFixed(1)-item[1].quick_status.sellPrice.toFixed(1)).toFixed(1)}</td>
                                 </tr>
                             )
                         }
